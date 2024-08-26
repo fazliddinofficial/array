d810790853d2485503df7904a2d16eec13a527da
fazliddinofficial <fazliddinquvatboyev8@gmail.com>
2024-08-26 04:55:37 -0700

    errors have been solved

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 (async function () {
-    let request = await Promise.all([
+    let request = await Promise.allSettled([
         fetch('https://jsonplaceholder.typicode.com/comments'),
         fetch('https://jsonplaceholder.typicode.com/comments'),
         fetch('https://jsonplaceholder.typicode.com/comments'),
         fetch('https://jsonplaceholder.typicode.com/comments'),
     ]);
-    let arr = {};
-    let freeObj = [];
-    for (let i = 0; i < 4; i++) {
-        arr[i] = await request[i].json();
-        for (let j = 0; j < 500; j++) {
-            freeObj[j] = arr[i][j];
-        }
-    }
-    console.log(freeObj);
+    console.log(request);
+
+    const res = await Promise.all(request.map(({ status, value }) => {
+        if (status === 'fulfilled') {
+            return value?.json();
+        }  
+    }));
+    console.log(res.flat(1));
 
 })();
 
